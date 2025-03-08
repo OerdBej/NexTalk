35ca2b3f36d44df122402f10a2345df59e9986a2
oerdbej <oerdbej@gmail.com>
2025-03-08 15:28:36 +0100

    new user and utils function for the token

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,21 +1,36 @@
 import User from "../models/user-model"
+import bcrypt from "bcrypt"
 
-export const signup = async (req,res) => {
-    const {email, fullName, password} = req.body
+export const signup = async (req, res) => {
+    const { email, fullName, password } = req.body
     try {
-        if(password.length < 6) {
-            return res.status(400).json({message: "Password must be at least 6 characters"})
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters" })
         }
-        const user = await User.findOne({email})
-        if(user) return res.status(400).json({message: "User already exists"})
+        const user = await User.findOne({ email })
+        if (user) return res.status(400).json({ message: "User already exists" })
+        const salt = await bcrypt.genSalt(10)
+        // hashedPassword is not readable for the db
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const newUser = new User({ email, fullName, password: hashedPassword })
+
+
+        if(newUser) {
+            // here we need to generate a token
+        } else {
+            res.status(400).json({message:'Invalid User Data'})
+        }
+
+
+
     } catch (error) {
     }
- }
+}
 
-export const login = (req,res) => {
+export const login = (req, res) => {
     res.send("login controller route")
 }
 
-export const logout = (req,res) => {
+export const logout = (req, res) => {
     res.send("logout controller route")
 }
---
+++ backend/src/utils/utils.js
@@ -0,0 +1,8 @@
+ import jwt from "jsonwebtoken"
+export const generateToken =  (userId, res) => {
+  console.log("generateToken")
+
+
+}
+
+
