11b51e7389216b04288dcc30bf3512af1bb4e4d7
oerdbej <oerdbej@gmail.com>
2025-03-12 20:46:54 +0100

    blah minor change

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import {signup, login, logout} from "../controllers/auth.js"
 import { protectRouter } from "../middlewares/authentication-middleware.js"
 import { updateProfile } from "../controllers/auth.js"
-
+import { checkAuth } from "../controllers/auth.js"
 
 
 router.post("/signup",signup )
@@ -13,6 +13,7 @@ router.post("/logout",logout )
 //appending route, if user want to update profile FIRST let's check if user is logged in
 router.put("/update-profile", protectRouter, updateProfile)
 
+//check if the user is authenticated -> profile page or login page depend on it
 router.get("/check", protectRouter,checkAuth)
 
 export default router;
